506801494364c1f3b6f3e7158cd4e246bf0d0da9
Marlon Raskin <marlonjraskin@gmail.com>
2020-06-18 21:23:48 -0400

    Added map, circle image, and stack to detail view

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -11,8 +11,27 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
 		VStack {
-			Text("Hello, World!")
-			Text("Yes!")
+			MapView()
+				.frame(height: 300)
+
+			CircleImage()
+				.offset(y: -130)
+				.padding(.bottom, -130)
+
+			VStack(alignment: .leading) {
+				Text("Turtle Rock")
+					.font(.title)
+				HStack {
+					Text("Joshua Tree National Park")
+						.font(.subheadline)
+					Spacer()
+					Text("California")
+						.font(.subheadline)
+				}
+			}
+			.padding()
+
+			Spacer()
 		}
     }
 }
