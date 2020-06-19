59fec2e6150aa52ceccf2cf59c0201e7e345010f
Marlon Raskin <marlonjraskin@gmail.com>
2020-06-18 23:07:34 -0400

    Forgot something...landmarkData array

--- Landmarks/Models/Data.swift
+++ Landmarks/Models/Data.swift
@@ -10,7 +10,7 @@ import UIKit
 import SwiftUI
 import CoreLocation
 
-//let landmarkData: [Landmark] =
+let landmarkData: [Landmark] = load("landmarkData.json")
 
 func load<T: Decodable>(_ filename: String) -> T {
 	let data: Data
